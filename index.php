0e24888128323e85386ff729a119f9e97eb31756
kolyanok <Kolya@MacBook-Pro-Kola.local>
2015-12-18 18:03:38 +0300

    Zalivaem

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php require('header.php'); ?>
+<div>
+    <ul class="breadcrumb">
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Dashboard</a>
+        </li>
+    </ul>
+</div>
+<div class="row">
+    <div class="box col-md-6">
+        <div class="box-inner">
+            <div class="box-header well" data-original-title="">
+                <h2><i class="glyphicon glyphicon-th"></i> Grid 6</h2>
+                <div class="box-icon">
+                    <a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
+                    <a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
+                    <a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
+                </div>
+            </div>
+            <div class="box-content">
+                <div class="row">
+                    <h1 style="padding-left:9px;"> <a href="1.php">6-7 класс</a> </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+    <div class="box col-md-6">
+        <div class="box-inner">
+            <div class="box-header well" data-original-title="">
+                <h2><i class="glyphicon glyphicon-th"></i> Grid 6</h2>
+                <div class="box-icon">
+                    <a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
+                    <a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
+                    <a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
+                </div>
+            </div>
+            <div class="box-content">
+                <div class="row">
+                    <h1 style="padding-left:9px;"> <a href="2.php"> 8-9 класс </a> </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+</div>
+<?php require('footer.php'); ?>
